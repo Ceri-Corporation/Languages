304de80520cc0906565c5e4857a46eef890e87dc
çeri <norchfatiheker@gmail.com>
2024-08-30 19:00:40 +0300

    refactored a bit

--- Brainfuck/Rust/src/main.rs
+++ Brainfuck/Rust/src/main.rs
@@ -1,11 +1,9 @@
-use std::fs::OpenOptions;
 use std::io::{Read, stdin};
 use crate::Token::{Dec, In, Inc, Left, Loop, Out, Right};
 
 fn main() {
-    let args:Vec<String> = std::env::args().collect();
-    let mut code = String::new();
-    OpenOptions::new().read(true).open(&args[0]).unwrap().read_to_string(&mut code).unwrap();
+    let args: Vec<String> = std::env::args().collect();
+    let code = std::fs::read_to_string(&args[0]).unwrap();
     interpret(lex_parse(&code));
 }
 
@@ -13,15 +11,14 @@ fn main() {
 fn interpret(code:Vec<Token>){
     let mut mem = [0;30000];
     let mut cursor = 0;
-    if code.contains(&In){
+    let mut input = if code.contains(&In) {
         let mut input_alt = String::new();
         stdin().read_line(&mut input_alt).unwrap();
-        let mut input = input_alt.bytes().collect();
-        int_all(&mut mem,&mut cursor,&code,&mut input);
-    }else {
-        let mut input = vec![];
-        int_all(&mut mem,&mut cursor,&code,&mut input);
-    }
+        input_alt.bytes().collect()
+    } else {
+        vec![]
+    };
+    int_all(&mut mem, &mut cursor, &code, &mut input);
 
 
 
@@ -33,7 +30,7 @@ fn int_all(mem:&mut [u8;30000],cursor:&mut usize,code:&Vec<Token>,input:&mut Vec
             Dec => mem[*cursor] = mem[*cursor].wrapping_sub(1),
             Right => *cursor = (*cursor +1) % 30000,
             Left => *cursor = (*cursor + 29999) %30000,
-            In => {mem[*cursor] = input[0];*input = input[1..].to_vec()}
+            In => {mem[*cursor] = input[0]; mem[*cursor] = input.remove(0);}
             Out => {print!("{}",mem[*cursor] as char)}
             Loop(c) => {
                 while mem[*cursor] != 0 {
@@ -49,11 +46,9 @@ fn lex_parse(s:&str)->Vec<Token>{
 }
 fn lex_parse_alt(s:&str,i:&mut usize)->Vec<Token>{
     let mut result = vec![];
-    let mut v:Vec<char> = s.chars().collect();
-    loop {
-        if *i == v.len(){
-            break;
-        }
+    let  v:Vec<char> = s.chars().collect();
+    while *i == v.len(){
+
         match v[*i] {
             '+' => result.push(Inc),
             '-' =>result.push(Dec),
@@ -61,11 +56,11 @@ fn lex_parse_alt(s:&str,i:&mut usize)->Vec<Token>{
             '<' =>result.push(Left),
             ',' =>result.push(In),
             '.' =>result.push(Out),
-            '[' =>  { let mut inr = 1usize;
+            '[' =>  { let mut inr = 1;
                 result.push(Loop(lex_parse_alt(&s[*i..],&mut inr)));
                 *i+=inr;
             },
-            ']' => {return result }
+            ']' => return result ,
             _ => {}
 
         }
